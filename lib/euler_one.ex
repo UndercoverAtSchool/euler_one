47f720063f9cc7af5a607e6fb2e8116eaf2d2f25
Sahil Deshmukh <hello@sahil.ink>
2026-01-09 22:22:32 +0530

    Initial commit

---
+++ lib/euler_one.ex
@@ -0,0 +1,18 @@
+defmodule EulerOne do
+  @moduledoc """
+  Documentation for `EulerOne`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EulerOne.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/euler_one_test.exs
@@ -0,0 +1,8 @@
+defmodule EulerOneTest do
+  use ExUnit.Case
+  doctest EulerOne
+
+  test "greets the world" do
+    assert EulerOne.hello() == :world
+  end
+end
